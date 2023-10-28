57d7bef4623baf67230deabaea0334bf81494c0a
Trần Quang Trí <tranwangtristudy@gmail.com>
2023-10-28 20:28:28 +0700

    Create Header

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <span>PHUONG MANH TU TECHNOLOGY Co. LTD.</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#D02023",
+        accent: "#313131",
+        accent2: "#D2D2D2",
+      },
+    },
   },
   plugins: [],
-}
-
+};
